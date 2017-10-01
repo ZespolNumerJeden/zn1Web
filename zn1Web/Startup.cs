867413800b6d53ab6b2777672ad02febe6607ca1
Arkadiusz Gutkowski <Arkadiusz Gutkowski>
2017-10-01 19:02:19 +0200

    Add project files.

---
+++ zn1Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(zn1Web.Startup))]
+namespace zn1Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
